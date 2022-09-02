8d028962605935cbb3d1557c8a38b1f3c23c9482
yuvanis <uvnistratov@gmail.com>
2022-09-02 15:28:24 +0300

    Create boat components

---
+++ src/lwc/boatSearch/boatSearch.js
@@ -0,0 +1,9 @@
+/**
+ * Created by yurynistratau on 1.09.22.
+ */
+
+import { LightningElement } from 'lwc';
+
+export default class BoatSearch extends LightningElement {
+
+}
---
+++ src/lwc/boatSearchResults/boatSearchResults.js
@@ -0,0 +1,9 @@
+/**
+ * Created by yurynistratau on 2.09.22.
+ */
+
+import { LightningElement } from 'lwc';
+
+export default class BoatSearchResults extends LightningElement {
+
+}
