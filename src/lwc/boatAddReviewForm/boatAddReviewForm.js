9c382eb84a5ba157ce55a6376ca4236f5a4c0071
yuvanis <uvnistratov@gmail.com>
2022-09-06 18:52:34 +0300

    Complete task

---
+++ src/lwc/boatAddReviewForm/boatAddReviewForm.js
@@ -0,0 +1,9 @@
+/**
+ * Created by yurynistratau on 6.09.22.
+ */
+
+import { LightningElement } from 'lwc';
+
+export default class BoatAddReviewForm extends LightningElement {
+
+}
---
+++ src/lwc/boatReviews/boatReviews.js
@@ -0,0 +1,9 @@
+/**
+ * Created by yurynistratau on 6.09.22.
+ */
+
+import { LightningElement } from 'lwc';
+
+export default class BoatReviews extends LightningElement {
+
+}
